c057325684f5723c2d8ad36ac601de630d520c88
Akansh Gulati <akansh.gulati@wingify.com>
2018-05-25 00:27:44 +0530

    fix(GDPRAssistant): Handled more cases. \nAdd support of help, repeat and stop

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-'use strict'
+'use strict';
 
-const {WebhookVerified} = require('jovo-framework')
-const {app} = require('./app/app.js')
+const {WebhookVerified} = require('jovo-framework');
+const {app} = require('./app/app.js');
 
 // =================================================================================
 // Server Configuration
 // =================================================================================
 
 if (app.isWebhook()) {
-  const port = process.env.PORT || 3000
-  WebhookVerified.listen(port, () => {
-    console.log(`Example server listening on port ${port}!`)
-  })
-  WebhookVerified.post('/webhook', (req, res) => {
-    app.handleWebhook(req, res)
-  })
+    const port = process.env.PORT || 3000;
+    WebhookVerified.listen(port, () => {
+        console.log(`Example server listening on port ${port}!`);
+    });
+    WebhookVerified.post('/webhook', (req, res) => {
+        app.handleWebhook(req, res);
+    });
 }
 
 exports.handler = (event, context, callback) => {
-  app.handleLambda(event, context, callback)
-}
+    app.handleLambda(event, context, callback);
+};
